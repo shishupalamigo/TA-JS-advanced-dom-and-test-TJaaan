d445e69c3ceb2d43ab9519440f05033b456d9055
Shishupal Kumar <shishupalkr2595@gmail.com>
2021-06-02 14:06:36 +0530

    Rafactor Drag And Drop | Assignment: DRAG AND DROP HTML5

--- block-BJaait/code/index.js
+++ block-BJaait/code/index.js
@@ -1,68 +1,74 @@
 
+var btn = document.querySelector('.add');
+var remove = document.querySelector('.draggable');
 
-let form = document.querySelector("#form");
-let draggables = document.getElementsByClassName('draggable')
-let display = document.querySelector('.display');
+function dragStart(e) {
+  this.style.opacity = '0.4';
+  dragSrcEl = this;
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', this.innerHTML);
+};
 
-
-form.addEventListener("submit",(event)=>{
-    event.preventDefault();
-    let title = document.querySelector("#title").value;
-    createUI(title);
-});
-
-function createUI(title){
-    document.querySelector("#title").value="";
-    let li = document.createElement("li");
-    li.innerText = title;
-    li.style.fontSize = "1.5rem";
-    let p = document.createElement("p");
-    p.innerText = "Drag Me";
-    p.style.fontSize = "0.5rem";
-    li.setAttribute("draggable","true");
-    li.setAttribute("class","draggable");
-    li.append(p);
-    display.append(li);
-    display.style.display = "flex";
-    display.style.justifyContent = "space-between"; 
-
-    [...draggables].forEach(draggable => {
-        draggable.addEventListener('dragstart', () => {
-          draggable.classList.add('dragging');
-        })
-      
-        draggable.addEventListener('dragend', () => {
-          draggable.classList.remove('dragging');
-        })
-      })
+function dragEnter(e) {
+  this.classList.add('over');
 }
 
+function dragLeave(e) {
+  e.stopPropagation();
+  this.classList.remove('over');
+}
 
+function dragOver(e) {
+  e.preventDefault();
+  e.dataTransfer.dropEffect = 'move';
+  return false;
+}
 
-  let containers = document.querySelectorAll('.container');
-  containers.forEach(container => {
-    container.addEventListener('dragover', e => {
-      e.preventDefault();
-      const afterElement = getDragAfterElement(container, e.clientY)
-      const draggable = document.querySelector('.dragging');
-      if (afterElement == null) {
-        container.appendChild(draggable);
-      } else {
-        container.insertBefore(draggable, afterElement);
-      }
-    })
-  })
-  
-  function getDragAfterElement(container, y) {
-    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')];
-  
-    return draggableElements.reduce((closest, child) => {
-      const box = child.getBoundingClientRect();
-      const offset = y - box.top - box.height / 2;
-      if (offset < 0 && offset > closest.offset) {
-        return { offset: offset, element: child };
-      } else {
-        return closest;
-      }
-    }, { offset: Number.NEGATIVE_INFINITY }).element;
+function dragDrop(e) {
+  if (dragSrcEl != this) {
+    dragSrcEl.innerHTML = this.innerHTML;
+    this.innerHTML = e.dataTransfer.getData('text/html');
   }
+  return false;
+}
+
+function dragEnd(e) {
+  var listItens = document.querySelectorAll('.draggable');
+  [].forEach.call(listItens, function(item) {
+    item.classList.remove('over');
+  });
+  this.style.opacity = '1';
+}
+
+function addEventsDragAndDrop(el) {
+  el.addEventListener('dragstart', dragStart, false);
+  el.addEventListener('dragenter', dragEnter, false);
+  el.addEventListener('dragover', dragOver, false);
+  el.addEventListener('dragleave', dragLeave, false);
+  el.addEventListener('drop', dragDrop, false);
+  el.addEventListener('dragend', dragEnd, false);
+}
+
+var listItems = document.querySelectorAll('.draggable');
+
+[].forEach.call(listItems, function(item) {
+  addEventsDragAndDrop(item);
+});
+
+function addNewItem() {
+  var newItem = document.querySelector('.input').value;
+  if (newItem != '') {
+    document.querySelector('.input').value = '';
+    var li = document.createElement('li');
+    var attr = document.createAttribute('draggable');
+    var ul = document.querySelector('ul');
+    li.className = 'draggable';
+    attr.value = 'true';
+    li.setAttributeNode(attr);
+    li.appendChild(document.createTextNode(newItem));
+    ul.appendChild(li);
+    addEventsDragAndDrop(li);
+  }
+}
+
+btn.addEventListener('click', addNewItem);
